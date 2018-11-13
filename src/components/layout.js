6baf95bc6c7889a7ca798abf1cb469f73dd64276
David Johnston <david@blacksheepcode.com>
2018-11-13 17:25:36 +1100

    Hopefully fix FOUC

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,21 +2,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
-import { Link } from 'gatsby'
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import pink from '@material-ui/core/colors/pink';
-import blue from '@material-ui/core/colors/blue';
-import CssBaseline from '@material-ui/core/CssBaseline';
 
 import Header from './header'
 import { withStyles } from '@material-ui/core';
 
-const theme = createMuiTheme({
-  palette: {
-    primary: pink,
-    secondary: blue
-  }
-});
+
 
 const Layout = ({ children, classes }) => (
   <StaticQuery
@@ -31,23 +21,21 @@ const Layout = ({ children, classes }) => (
     `}
     render={data => (
       <>
-        <MuiThemeProvider theme={theme}>
-          <CssBaseline />
-          <Helmet
-            title={data.site.siteMetadata.title}
-            meta={[
-              { name: 'description', content: 'Sample' },
-              { name: 'keywords', content: 'sample, something' },
-            ]}
-          >
-            <html lang="en" />
-          </Helmet>
-          <Header siteTitle={data.site.siteMetadata.title} />
-          <div className={classes.body}
-          >
-            {children}
-          </div>
-        </MuiThemeProvider>
+
+        <Helmet
+          title={data.site.siteMetadata.title}
+          meta={[
+            { name: 'description', content: 'Sample' },
+            { name: 'keywords', content: 'sample, something' },
+          ]}
+        >
+          <html lang="en" />
+        </Helmet>
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <div className={classes.body}
+        >
+          {children}
+        </div>
       </>
     )}
   />
