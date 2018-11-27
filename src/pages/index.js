8cf23982dc42d7561e6a2d3cdd8b3aeb02404216
David Johnston <david@blacksheepcode.com>
2018-11-28 09:34:06 +1100

    Add contact details

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,11 @@ import { Link } from 'gatsby'
 import withRoot from "../withRoot";
 import Layout from '../components/layout'
 import Image from '../components/image'
-
+import P from "../components/generic/typography/P";
 const IndexPage = () => (
   <>
+    <P> If you're interested in getting involved - please contact us at <a href="mailto:mwcc@blacksheepcode.com">mwcc@blacksheepcode.com</a> </P>
+
     <Image name="homepage" />
 
   </>
